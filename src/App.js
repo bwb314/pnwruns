5fa1d26cbe9e0ef3a3aff1d468948666ee864860
Brandon Bakr <brandon.bakr@gmail.com>
2023-08-13 12:21:29 -0700

    Starting React App

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './images/pnwrunslogo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <head>
+        <title>RRR</title>
+      </head>
+      <link rel="icon" type="image/x-icon" href="images/pnwrunsfavicon.png"></link>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
